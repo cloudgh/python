f672a0d5273740ddce5205d37a0f353e9d1ff7c6
cloudgh <92978374+cloudgh@users.noreply.github.com>
2023-11-25 22:32:53 +0700

    Update Untitled11.py

--- Untitled11.py
+++ Untitled11.py
@@ -9,12 +9,11 @@ class Customer:
         self.customer_id = customer_id
         self.name = name
 
-class ShoppingCart:
-    def __init__(self):
-        self.items = []
-
-    def add_item(self, product, quantity):
-        self.items.append({"product": product, "quantity": quantity})
+class Order:
+    def __init__(self, customer, items):
+        self.customer = customer
+        self.items = items
+        self.total = self.calculate_total()
 
     def calculate_total(self):
         total = 0
@@ -22,18 +21,29 @@ class ShoppingCart:
             total += item["product"].price * item["quantity"]
         return total
 
-    def checkout(self, customer):
-        total = self.calculate_total()
-        print(f"Checkout for {customer.name}:")
+    def display_order(self):
+        print(f"Order for {self.customer.name}:")
         for item in self.items:
             print(f"{item['product'].name} - {item['quantity']} units")
-        print(f"Total: ${total}")
+        print(f"Total: ${self.total}")
         print("Thank you for shopping with us!")
+
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, product, quantity):
+        self.items.append({"product": product, "quantity": quantity})
+
+    def checkout(self, customer):
+        order = Order(customer, self.items)
+        order.display_order()
 
 
 product1 = Product(1, "Laptop", 1200)
 product2 = Product(2, "Smartphone", 800)
 product3 = Product(3, "Headphones", 100)
+
 
 customer = Customer(1, "John Doe")
 
@@ -44,5 +54,6 @@ cart = ShoppingCart()
 cart.add_item(product1, 2)
 cart.add_item(product2, 1)
 cart.add_item(product3, 3)
+
 
 cart.checkout(customer)
