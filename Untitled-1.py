766ebb883a12dc81de7ad316567fc954fb0fabf5
cloudgh <92978374+cloudgh@users.noreply.github.com>
2023-12-14 22:47:08 +0700

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,17 +1,32 @@
-def convert_date_to_dict(date_str):
+import random
 
-    year, month, day = date_str.split('-')
-    
-
-    date_dict = {
-        'year': year,
-        'month': month,
-        'day': day
-    }
-    
-    return date_dict
+with open("random_numbers.txt", "w") as file:
+    for _ in range(100000):
+        file.write(str(random.randint(1, 1000000)) + "\n")
 
 
-date_str = '2023-11-11'
-result_dict = convert_date_to_dict(date_str)
-print(result_dict)
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        mid_value = arr[mid]
+
+        if mid_value == target:
+            return mid
+        elif mid_value < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+with open("random_numbers.txt", "r") as file:
+    data = [int(line.strip()) for line in file]
+
+target_element = random.choice(data)
+
+
+sorted_data = sorted(data)
+binary_result = binary_search(sorted_data, target_element)
+print(f"Binary Search: Element {target_element} found at index {binary_result}")
